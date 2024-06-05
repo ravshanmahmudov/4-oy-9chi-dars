5f5fe9c32ebf501062d7dd760deaebd449863690
ravshanmahmudov <ravshanmahmudov882@gmail.com>
2024-06-06 01:34:09 +1400

    massalar

---
+++ js/index.js
@@ -0,0 +1,171 @@
+// 1chi masala
+//   let boshMasiv =[];
+// while(boshMasiv.length<5){
+// boshMasiv.push(boshMasiv.length+1)
+// }
+// console.log(boshMasiv);
+
+
+// 2chi masala
+
+
+// let boshMasiv =[];
+// boshMasiv.push('arg1');
+// boshMasiv.push(' arg2');
+// boshMasiv.push('arg3');
+// boshMasiv.pop("");
+// let birlashMassiv=boshMasiv.join('');
+// console.log(birlashMassiv);
+
+
+
+// 3chi masala
+
+// let boshMassiv = []; 
+
+// boshMassiv.unshift('element4','element3','element2','element1');
+
+// boshMassiv.reverse();
+// let uzunligi = boshMassiv.length;
+// console.log(boshMassiv);
+// console.log(uzunligi); 
+
+
+// 4chi masala
+
+// let boshMassiv = []; 
+// boshMassiv.unshift('arg1', 'arg2')
+// boshMassiv.shift();
+// let stiringda=boshMassiv.join(",");
+// console.log(stiringda);7
+
+
+// 5chi masala
+
+// let massiv = [1,2,3,4,5,6,7]; 
+// massiv.pop();
+// let elementSoni = massiv.length;
+// var birinchiElement = massiv[0];
+// console.log(elementSoni);
+// console.log(birinchiElement);
+
+// 6chi masala
+
+// let berilganMassiv = [1,2,3,4];
+// berilganMassiv.push(5);
+// let oxirgiElement = berilganMassiv.at(-1); 
+// console.log(oxirgiElement); 
+
+
+// 7chi masala
+
+
+// let berilganMassiv = [1,2,3,4,5,6];
+// berilganMassiv.unshift(7);
+// berilganMassiv.reverse();
+// let massivUzunligi = berilganMassiv.length;
+// console.log(berilganMassiv); 
+// console.log( massivUzunligi); 
+
+// 8chi masala
+
+
+// let berilganMassiv = ['gilos','olma', 'banan', 'nok' ];
+// berilganMassiv.shift();
+// let qolganSon = berilganMassiv.length;
+// let massivString = berilganMassiv.join(', '); 
+// console.log( qolganSon); 
+// console.log( massivString); 
+
+// 9chi  masala
+
+// let berilganMassiv = [1,2,3];
+// berilganMassiv.push(7,8);
+// berilganMassiv.unshift(10);
+// let massivUzunligi = berilganMassiv.length;
+// berilganMassiv.reverse();
+// let massivStringi = berilganMassiv.join(' ');
+// console.log( massivUzunligi); 
+// console.log( massivStringi);
+
+// 10chi masala
+
+// let berilganMassiv = [1,2,3,4];
+// berilganMassiv.shift();
+// berilganMassiv.pop();
+// let qolganUzunligi = berilganMassiv.length;
+// let teskariMassiv = berilganMassiv.reverse();
+// console.log( qolganUzunligi);
+// console.log( teskariMassiv);
+
+
+// 11chi masala
+
+// let berilganMassiv = [1,2,3,4];
+// berilganMassiv.toString()
+// let uzunligi=berilganMassiv.length
+// let stiring =berilganMassiv.reverse();
+// console.log(stiring);
+// console.log(uzunligi);
+
+
+// 12 masala
+
+// let berilganMassiv = [1,2,3,4];
+// let berilganMassiv2 =[ 7,8,9];
+// let arr2 = berilganMassiv.slice(1,3);
+// let arr3 = berilganMassiv2.slice(0,2);
+// console.log(arr2);
+// console.log(arr3);
+// console.log(berilganMassiv.concat(berilganMassiv2).length);
+
+
+// 13chi masala
+
+// let berilganMassiv = [1,2,3,4,5,6,7,8,9];
+// berilganMassiv.splice(2,3);
+// let arr = berilganMassiv.join(",");
+// console.log(berilganMassiv.length);
+// console.log(arr);
+
+// 14masala
+
+// let berilganMassiv = [1,2,3,4,5,6,7,8,9];
+// let arr = berilganMassiv.slice(3,6,0)
+// let arr1 = arr.reverse();
+// console.log(arr1);
+
+// 15chi masala
+
+// let berilganMassiv = [1,2,3,4,[5,6,7,8,9]];
+// let arr = berilganMassiv.flat()
+// console.log(arr.at(-1));
+
+// 16chi masala
+
+
+// let berilganMassiv = [1,2,3,4,5,6];
+// let arr=berilganMassiv.at(3)
+// let arr1=berilganMassiv.unshift(arr)
+// console.log(berilganMassiv);
+
+// 17chi masala
+
+// let berilganMassiv = [7,2,3,'salom','xayr'];
+// let arr = berilganMassiv.reverse();
+// let arr1 = arr.toString()
+// console.log(arr1);
+// console.log("uzunligi",arr1.at(-1));
+
+// 18chi masala
+
+// let berilganMassiv = [1,2,3,4,5,6,7,8,9];
+// let arr=berilganMassiv.slice(0,2)
+// console.log(berilganMassiv.splice(4, 1));
+
+// 19 chi masala
+
+// let berilganMassiv =[1,2,3,4,5,6,7,8]
+// let arr1=berilganMassiv.slice(3,2,0)
+// console.log(arr1);
+
